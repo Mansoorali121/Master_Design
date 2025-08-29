71ecd38f33ba9aba2f4b30ce6c1f014989a9d862
Mansoor Sahito <mansoorsahito601@gmail.com>
2025-08-29 11:03:05 +0500

    Social Section Complete

--- src/screens/ContactUsScreen.js
+++ src/screens/ContactUsScreen.js
@@ -9,14 +9,21 @@ import SocialSection from '../components/SocialSection';
 
 const ContactUsScreen = () => {
   return (
-    <View style={{ marginTop: vs(20) , paddingHorizontal:s(17)}}>
+    <View style={{ marginTop: vs(20), paddingHorizontal: s(17) }}>
       <View style={styles.header}>
         <BackButton />
         <Userprofile />
       </View>
-  {/* Social Section Here  */}
-             <SocialSection/>
-
+      <Text style={styles.screenTitle}>Contact Us </Text>
+      {/* Social Section Here  */}
+      <View style={styles.socialContainer}>
+        <Text style={styles.socialtitle}>Social Media Platforms</Text>
+        <SocialSection />
+        <SocialSection />
+        <SocialSection />
+        <SocialSection />
+        <SocialSection />
+      </View>
     </View>
   );
 };
@@ -25,4 +32,21 @@ export default ContactUsScreen;
 
 const styles = StyleSheet.create({
   header: { flexDirection: 'row', justifyContent: 'space-between' },
+  socialContainer: {
+    backgroundColor: '#F5F5F5',
+    borderRadius: s(14),
+    paddingHorizontal: s(18),
+    paddingVertical: vs(15),
+    marginTop: vs(22),
+  },
+  socialtitle: {
+    fontSize: s(16),
+    fontWeight: 'semibold',
+  },
+  screenTitle: {
+    fontSize: s(30),
+    fontWeight: 'semibold',
+    marginTop: vs(20),
+    marginStart: s(19),
+  },
 });
